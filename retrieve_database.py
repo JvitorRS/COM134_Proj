d73c8a61c1fabf46ca17b2ad58d6bdcfd6cbfef8
Haotian-Yu-Ethan <yuhaotianyu1@gmail.com>
2026-08-27 12:01:15 +1000

    Add three functions in class READ and add a function in class Create_Database

--- retrieve_database.py
+++ retrieve_database.py
@@ -16,7 +16,11 @@ class Database_Operatjion(SPIStaffOutlineSystem):
 class Create_Database(Database_Operatjion):
     def __init__(self):
         super().__init__()
+        
+
+    def create_new_course(self,  course_name: str,  course_code: str):
         pass
+
 
 class Read_Database(Database_Operatjion):
     def __init__(self):
@@ -65,7 +69,7 @@ class Read_Database(Database_Operatjion):
         email(str): The email address of that user
 
         Returns:
-        list | None: The all relevant courses(str)
+        list | None: The all relevant course codes(str)
         """
         try:
             sql= """
@@ -76,7 +80,38 @@ class Read_Database(Database_Operatjion):
             self.cursor.execute(sql, (email,))
             result = self.cursor.fetchall()
 
-        # If nothing is found in database, more error type will be update in the future
+            # If nothing is found in database, more error type will be update in the future
+            if not result:
+                return None
+
+        except mysql.connector.Error as error:
+            print(f"Database error: {error}")
+            return None
+
+        else:
+            return result
+
+
+    def get_course_info(self, coursecode: str,  version: float) ->  str:
+        """
+        Retrieve the current version of that course
+
+        Args:
+        coursecode(str): The course code of taht course         [eg: BCOMP112]
+
+        Returns:
+        str | None: The current version of that course       [eg:  "2.12"]
+        """
+        try:
+            sql= """
+                SELECT ???, ???
+                FROM ???
+                WHERE coursecode = %s, version = %s;
+            """
+            self.cursor.execute(sql, (coursecode, version))
+            result = self.cursor.fetchone()
+
+            # If nothing is found in database, more error type will be update in the future
             if not result:
                 return None
 
@@ -102,12 +137,74 @@ class Read_Database(Database_Operatjion):
             sql= """
                 SELECT ???, ???
                 FROM ???
-                WHERE coursecode = %s
+                WHERE coursecode = %s;
             """
             self.cursor.execute(sql, (coursecode,))
             result = self.cursor.fetchall()
 
-        # If nothing is found in database, more error type will be update in the future
+            # If nothing is found in database, more error type will be update in the future
+            if not result:
+                return None
+
+        except mysql.connector.Error as error:
+            print(f"Database error: {error}")
+            return None
+
+        else:
+            return result
+
+
+    def get_current_course_version(self, coursecode: str) ->  float:
+        """
+        Retrieve the current version of that course
+
+        Args:
+        coursecode(str): The course code of taht course         [eg: BCOMP112]
+
+        Returns:
+        float | None: The current version of that course       [eg:  2.12]
+        """
+        try:
+            sql= """
+                SELECT ???, ???
+                FROM ???
+                WHERE coursecode = %s;
+            """
+            self.cursor.execute(sql, (coursecode,))
+            result = self.cursor.fetchone()
+
+            # If nothing is found in database, more error type will be update in the future
+            if not result:
+                return None
+
+        except mysql.connector.Error as error:
+            print(f"Database error: {error}")
+            return None
+
+        else:
+            return result
+
+
+    def get_current_course_history_log(self, coursecode: str) ->  list:
+        """
+        Retrieve all versions of that course
+
+        Args:
+        coursecode(str): The course code of taht course         [eg: BCOMP112]
+
+        Returns:
+        list(float) | None: All versions of that course       [eg:  [2.0, 1.3, 1.2, 1.1, 1.0]]
+        """
+        try:
+            sql= """
+                SELECT ???, ???
+                FROM ???
+                WHERE coursecode = %s;
+            """
+            self.cursor.execute(sql, (coursecode,))
+            result = self.cursor.fetchone()
+
+            # If nothing is found in database, more error type will be update in the future
             if not result:
                 return None
 
@@ -120,17 +217,18 @@ class Read_Database(Database_Operatjion):
 
 
 
-
-
-        
-
 class Update_Database(Database_Operatjion):
     def __init__(self):
         super().__init__()
-        pass
+        
+
+
+
+
+
 
 class Delete_Database(Database_Operatjion):
     def __init__(self):
         super().__init__()
-        pass
+        
 
